4cabefef26bfea66f473ab8681f6c19d3c665578
Nomolos29 <nomolos2019@gmail.com>
2024-09-29 14:47:35 +0100

    Updated account detail search and added icon

--- src/components/walletConnect.jsx
+++ src/components/walletConnect.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+
 import chains from "../../chains.json";
+import { LuWallet } from 'react-icons/lu';
 
 const WalletConnect = () => {
 
   const [balance, setBalance] = useState(null);
   const [account, setAccount] = useState([]);
   const [chainId, setChainId] = useState(null);
-  const [accountNouce, setAccountNouce] = useState(0)
+  const [accountNouce, setAccountNouce] = useState(0);
+  const [inputValue, setInputValue] = useState("");
   const [inputAddress, setInputAddress] = useState("");
   
 
@@ -48,11 +51,11 @@ const WalletConnect = () => {
       setBalance(accountBalance);
 
       handleAccountNouce(index);
+      setInputAddress(account[index]);
     } catch (error) {
       console.log(error.message);
     }
-    setInputAddress(account[index]);
-  }
+  } 
 
   const handleAccountNouce = async(index) => {
     try {
@@ -79,13 +82,33 @@ const WalletConnect = () => {
     handleWalletConnect();
   }
 
+
+  const handleGetBalance = (event) => {
+    event.preventDefault();
+
+    setInputAddress("");
+    setAccountNouce(null)
+
+    console.log(inputValue);
+    
+    let accountArray = [];
+    accountArray.push(inputValue)
+  
+    handleChangeInAccount(accountArray);
+    // setInputAddress(inputValue);
+
+
+    setInputValue("");
+
+  }
+
   ethereum.on('accountsChanged', handleChangeInAccount);
   ethereum.on('chainChanged', handleOnChainChange);
 
-  // ethereum.removeListner('accountsChanged', handleChangeInAccount)
+  // ethereum.removeListener('accountsChanged', handleChangeInAccount)
 
 
-  const cardContainer = " w-[400px] h-[400px]";
+  const cardContainer = " w-[420px] h-[400px]";
 
   
 
@@ -97,10 +120,18 @@ const WalletConnect = () => {
       <span className='flex items-center justify-center gap-x-10'>
 
         <section className={`${cardContainer} shadow-lg border-[1px] overflow-hidden rounded-xl p-5 flex flex-col justify-between items-start`}>
-          <h1 className='text-4xl font-bold text-gray-400'>Accounts</h1>
+          <div className='flex flex-col w-full'>
+            <h1 className='text-4xl font-bold text-gray-400'>Accounts</h1>
+            <form onSubmit={handleGetBalance} className='flex justify-between w-full gap-x-3'>
+              <input 
+              value={inputValue} 
+              onChange={(e) => setInputValue(e.target.value)}
+              type="text" className='w-full px-2 outline-none bg-gray-300 rounded-lg' /><button type='submit' className='w-[50%] bg-gray-300 py-1 rounded-lg px-2 text-center'>check balance</button>
+            </form>
+          </div>
           <main className='w-full space-y-3'>
             {account.map((address, index) => (
-              <div key={index} onClick={() => handleBalanceCheck(index)}className='w-full cursor-pointer px-4 py-2 font-medium text-sm shadow-md rounded-lg'>{address}</div>
+              <div key={index} onClick={() => handleBalanceCheck(index)}className={`w-full cursor-pointer px-3 py-2 font-medium text-sm shadow-md flex justify-between items-center rounded-lg ${address == "" ? "bg-red-500 text-gray-50" : ""}`}>{address == "" ? "Please input an Address to check it's balance" : address}<span><LuWallet /></span></div>
             ))}
             <button onClick={handleWalletConnect} className='text-xl font-medium py-2 px-4 bg-white/50 text-gray-500 shadow-md border-[1px] hover:bg-gray-400 hover:text-gray-50 rounded-lg'>Connect Wallet</button>
           </main>
